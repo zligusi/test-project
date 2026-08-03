b43342d3e358292b53ccdcbaf61da165848bba37
zligusi <tort24abcd@gmaill.com>
2026-08-03 12:58:21 +0200

    configure admin.py

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Category , Size , Product ,  \
+    ProductSize , ProductImage 
 
-# Register your models here.
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+      
+class ProductSizeInline(admin.TabularInline):
+    model = ProductSize
+    extra = 1
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name' , 'category','color','price']
+    list_filter = ['category' , 'color']
+    search_fields = ['name', 'color' , 'description']
+    prepopulated_fields = {'slug':('name',)}
+    inlines = [ProductSizeInline , ProductImageInline]
+    
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name' , 'slug']
+    prepopulated_fields = {'slug':('name',)}
+    
+class SizeAdmin(admin.ModelAdmin):
+    list_display = ['name' ]
+    
+admin.site.register(Category , CategoryAdmin)
+admin.site.register(Size , SizeAdmin)
+admin.site.register(Product , ProductAdmin)
